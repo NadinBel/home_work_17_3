93d1429bb527dcc156c17cb1f3c83d737e7c51c6
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-10-29 01:09:58 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -12,7 +12,7 @@ class User(Base):
     lastname = Column(String)
     age = Column(Integer)
     slug = Column(String, unique=True, index=True)
-    tasks = relationship('Task', back_populates='user')
+    task = relationship('Task', back_populates='user')
 
 
 from sqlalchemy.schema import CreateTable
